e46a3a0f7ffb40607cfdcacc235e5c3c7952788a
Juan Charparin <juan.charparin@zaldivar.com>
2025-08-22 17:20:26 -0300

    bkups

--- app/main.py
+++ app/main.py
@@ -1,24 +1,36 @@
-from datetime import datetime
-
-
+from pydoc import text
 from flask import Flask, request, jsonify, render_template
-
+import mysql.connector
 
 app = Flask(__name__)
 
+conection_string = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="Juanki3031$$",
+    database="crm"
+)
+
+cursor = conection_string.cursor()
 
 
-#Ruta verificar funcionamiento correcto de la API
-@app.route('/api/status', methods=['GET'])
-def status():
-    return jsonify({'status': 'API en funcionamiento'}), 200
 
 
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    cursor.execute("SELECT * FROM locales")
+    rows = cursor.fetchall()
+    return render_template("index.html", rows=rows)
 
+@app.route("/ping")
+def ping():
+    try:
+        cursor.execute("SELECT * FROM locales")
+        rows = cursor.fetchall()
+        return '<br>'.join(str(row) for row in rows)
+    except Exception as e:
+        return f"Error: {str(e)}"
 
 
 
